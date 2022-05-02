f96f74ff5be55bfb9e487a22ad2ee22dc202510e
marzeq <58303665+marzeq@users.noreply.github.com>
2022-05-02 12:22:50 +0200

    improve statusbar

--- sway/statusbar.sh
+++ sway/statusbar.sh
@@ -1,8 +1,24 @@
 while true
 do
-  DATE="$(date +'%Y-%m-%d %H:%M:%S')"
+  DATE="$(date +'%a %d/%m/%Y %H:%M:%S')"
   VOLUME="$(pamixer --get-volume-human)"
+  VOLUMEVALUE=${VOLUME::-1}
+  VOLUMEICON=""
+  VOLUMETEXT="$VOLUME"
 
-  echo "墳 $VOLUME |   $DATE "
-  sleep 1
+  if (( $VOLUMEVALUE == 0 )); then
+    VOLUMETEXT="Muted"
+    VOLUMEICON="婢"
+  elif (( $VOLUMEVALUE >= 75)); then
+    VOLUMEICON=""
+  elif (( $VOLUMEVALUE >= 50 )); then
+    VOLUMEICON="墳"
+  elif (( $VOLUMEVALUE >= 25 )); then
+    VOLUMEICON="奔"
+  elif (( $VOLUMEVALUE < 25 )); then
+    VOLUMEICON="奄"
+  fi
+  
+  echo "$VOLUMEICON $VOLUMETEXT |   $DATE "
+  sleep 0.1
 done
